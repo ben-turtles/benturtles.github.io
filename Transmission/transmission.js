3553412b21d22d1305262c85ae7062877327561b
ben-turtles <benepicturtles@gmail.com>
2025-04-21 21:08:31 -0400

    match

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -1,24 +1,19 @@
 const LOCATION_PASSWORD = "WPI"
 const PASSWORD_TIMEOUT = 1;
-let triedPassword = false;
 
 function checkPassword() {
-    if (!triedPassword) {
-        triedPassword = true;
-        let inputPassword = document.getElementById("location-password").value;
-        inputPassword = inputPassword.toLowerCase();
-        if (!(inputPassword.trim().length === 0)) {
-            // Not whitespace, check for correctness.
-            if (inputPassword === LOCATION_PASSWORD) {
-                // Matched password!
-                window.location = "location-found-394232";
-            } else {
-                // Wrong password! Reset field and add little delay.
-                document.getElementById("location-password").value = "";
-                setTimeout(function() {
-                    triedPassword = false;
-                }, PASSWORD_TIMEOUT * 1000);
-            }
+    let inputPassword = document.getElementById("location-password").value;
+    inputPassword = inputPassword.toLowerCase();
+    if (!(inputPassword.trim().length === 0)) {
+        // Not whitespace, check for correctness.
+        if (inputPassword === LOCATION_PASSWORD) {
+            // Matched password!
+            alert("yeah");
+            window.location = "location-found-394232";
+        } else {
+            // Wrong password! Reset field.
+            alert("no");
+            document.getElementById("location-password").value = "";
         }
     }
 }
