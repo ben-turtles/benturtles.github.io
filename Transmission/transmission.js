8cccbabfa5ec345e7af24ebca54517980642d63a
ben-turtles <benepicturtles@gmail.com>
2025-04-21 21:25:05 -0400

    ah

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -6,15 +6,15 @@ function checkPassword() {
     inputPassword = inputPassword.toLowerCase();
     if (!(inputPassword.trim().length === 0)) {
         // Not whitespace, check for correctness.
-        alert("hi: " + inputPassword);
+        alert("hi: " + inputPassword + ", " + inputPassword === LOCATION_PASSWORD);
         if (inputPassword === LOCATION_PASSWORD) {
-            // Matched password!
-            alert("yeah");
-            window.location = "location-found-394232";
+            // // Matched password!
+            // alert("yeah");
+            // window.location = "location-found-394232";
         } else {
-            // Wrong password! Reset field.
-            alert("no");
-            document.getElementById("location-password").value = "";
+            // // Wrong password! Reset field.
+            // alert("no");
+            // document.getElementById("location-password").value = "";
         }
     }
 }
