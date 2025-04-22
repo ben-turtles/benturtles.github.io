4edc670284ad606eb320628e4ccceee05020d734
ben-turtles <benepicturtles@gmail.com>
2025-04-21 22:45:58 -0400

    better

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -1,21 +1,19 @@
-const LOCATION_PASSWORD = "WPI"
+const LOCATION_PASSWORD = "wpi"
 const PASSWORD_TIMEOUT = 1;
 
 function checkPassword() {
     let inputPassword = document.getElementById("location-password").value;
-    // inputPassword = inputPassword.toLowerCase();
     if (!(inputPassword.trim().length === 0)) {
         // Not whitespace, check for correctness.
-        alert("COMPARE: " + inputPassword.toLowerCase().localeCompare("WPI"));
-        // if (inputPassword.valueOf() === LOCATION_PASSWORD) {
-        //     // // Matched password!
-        //     // alert("yeah");
-        //     window.location = "location-found";
-        // } else {
-        //     // // Wrong password! Reset field.
-        //     // alert("no");
-        //     document.getElementById("location-password").value = "";
-        // }
+        if (inputPassword.toLowerCase() === LOCATION_PASSWORD.toLowerCase()) {
+            // // Matched password!
+            // alert("yeah");
+            window.location = "location-found";
+        } else {
+            // // Wrong password! Reset field.
+            // alert("no");
+            document.getElementById("location-password").value = "";
+        }
         // alert("check: " + inputPassword.valueOf() + ", " + (inputPassword.valueOf() === LOCATION_PASSWORD) + "..." + inputPassword.length + ", "
     // + inputPassword.);
     }
