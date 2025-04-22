4b109832c2d7253fb91023b2bbf97fab34bc2a3f
ben-turtles <benepicturtles@gmail.com>
2025-04-21 22:37:56 -0400

    comp

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -3,18 +3,19 @@ const PASSWORD_TIMEOUT = 1;
 
 function checkPassword() {
     let inputPassword = document.getElementById("location-password").value;
-    inputPassword = inputPassword.toLowerCase();
+    // inputPassword = inputPassword.toLowerCase();
     if (!(inputPassword.trim().length === 0)) {
         // Not whitespace, check for correctness.
-        if (inputPassword.valueOf() === LOCATION_PASSWORD) {
-            // // Matched password!
-            // alert("yeah");
-            window.location = "location-found";
-        } else {
-            // // Wrong password! Reset field.
-            // alert("no");
-            document.getElementById("location-password").value = "";
-        }
+        alert("COMPARE: " + inputPassword.toLowerCase().localeCompare(LOCATION_PASSWORD));
+        // if (inputPassword.valueOf() === LOCATION_PASSWORD) {
+        //     // // Matched password!
+        //     // alert("yeah");
+        //     window.location = "location-found";
+        // } else {
+        //     // // Wrong password! Reset field.
+        //     // alert("no");
+        //     document.getElementById("location-password").value = "";
+        // }
         // alert("check: " + inputPassword.valueOf() + ", " + (inputPassword.valueOf() === LOCATION_PASSWORD) + "..." + inputPassword.length + ", "
     // + inputPassword.);
     }
