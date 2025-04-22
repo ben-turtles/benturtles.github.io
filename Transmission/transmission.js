226c41567beac8eb10dcd8c4838f4304fff7ab5d
ben-turtles <benepicturtles@gmail.com>
2025-04-21 21:45:42 -0400

    yea

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -6,7 +6,7 @@ function checkPassword() {
     inputPassword = inputPassword.toLowerCase();
     if (!(inputPassword.trim().length === 0)) {
         // Not whitespace, check for correctness.
-        alert("hi: " + inputPassword + ", " + inputPassword === LOCATION_PASSWORD);
+        alert("hi: " + inputPassword + ", " + (inputPassword === LOCATION_PASSWORD));
         if (inputPassword === LOCATION_PASSWORD) {
             // // Matched password!
             // alert("yeah");
