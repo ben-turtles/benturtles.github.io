cb97f03959cc3db18d0d02586a1b23a9a6e3876a
ben-turtles <benepicturtles@gmail.com>
2025-04-21 20:36:07 -0400

    click test

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -1,18 +1,5 @@
-window.onload = () => {
-    const pointerScroll = (elem) => {
-        const dragStart = (ev) => elem.setPointerCapture(ev.pointerId);
-        const dragEnd = (ev) => elem.releasePointerCapture(ev.pointerId);
-        const drag = (ev) => elem.hasPointerCapture(ev.pointerId) && (elem.scrollLeft -= ev.movementX);
-
-        elem.addEventListener("pointerdown", dragStart);
-        elem.addEventListener("pointerup", dragEnd);
-        elem.addEventListener("pointermove", drag);
-    }
-
-    document.querySelectorAll(".scroller").forEach(pointerScroll);
-}
-
-function checkPassword(x, y, code) {
-    alert("onclick() called");
-    window.location = "dev.html";
+function checkPassword() {
+    let inputPassword = document.getElementById("location-password").value;
+    alert("onclick(): " + inputPassword);
+    // window.location = "dev.html";
 }
