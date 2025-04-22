d835501a8948f784d149d35a3e96069cc4f5607e
ben-turtles <benepicturtles@gmail.com>
2025-04-21 21:01:50 -0400

    correct test

--- Transmission/transmission.js
+++ Transmission/transmission.js
@@ -1,16 +1,22 @@
 const LOCATION_PASSWORD = "test!"
+const PASSWORD_TIMEOUT = 3;
+let triedPassword = false;
 
 function checkPassword() {
-    let inputPassword = document.getElementById("location-password").value;
-    inputPassword = inputPassword.toLowerCase();
-    if (!(inputPassword.trim().length === 0)) {
-        // not whitespace
-        if (inputPassword === LOCATION_PASSWORD) {
-            alert("Success!");
-        } else {
-            alert("No :(");
+    if (!triedPassword) {
+        triedPassword = true;
+        let inputPassword = document.getElementById("location-password").value;
+        inputPassword = inputPassword.toLowerCase();
+        if (!(inputPassword.trim().length === 0)) {
+            // not whitespace
+            if (inputPassword === LOCATION_PASSWORD) {
+                window.location = "location-found-394232";
+            } else {
+                document.getElementById("location-password").value = "";
+                setTimeout(function() {
+                    triedPassword = false;
+                }, PASSWORD_TIMEOUT * 1000);
+            }
         }
     }
-    // alert("onclick(): " + inputPassword);
-    // window.location = "dev.html";
 }
